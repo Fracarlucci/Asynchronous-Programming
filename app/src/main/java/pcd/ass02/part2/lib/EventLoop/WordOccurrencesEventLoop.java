5b20ab1841c7be859934210a570b0fcbb6001076
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2024-05-11 15:42:10 +0200

    Update EventLoop implementation

--- app/src/main/java/pcd/ass02/part2/lib/EventLoop/WordOccurrencesEventLoop.java
+++ app/src/main/java/pcd/ass02/part2/lib/EventLoop/WordOccurrencesEventLoop.java
@@ -1,86 +1,50 @@
 package pcd.ass02.part2.lib.EventLoop;
 
+import io.vertx.core.json.JsonObject;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import pcd.ass02.part2.lib.WordOccurrences;
-
+import io.vertx.core.*;
 import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.concurrent.Future;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class WordOccurrencesEventLoop implements WordOccurrences {
+public class WordOccurrencesEventLoop extends AbstractVerticle implements WordOccurrences {
 
-    private final int nThreads;
+//    private final int nThreads;
     private int counter = 0;
     private String wordToFind;
 
+//    Map<> result;
 
-    private final Map<String, Integer> map = new HashMap<>();
+    private Map<String, Integer> result;
     private final Set<String> pageLinks = new HashSet<>();
     Document doc;
-    private final List<WordFinderThread> wordFinderThreads;
-//    private final Deque<Event> events;
-    private Monitor counterMonitor = new Monitor();
-    private Monitor linksMonitor = new Monitor();
-
-    public WordOccurrencesEventLoop(int nThreads) {
-//        this.events = new ArrayDeque<>();
-        this.wordFinderThreads = new LinkedList<>();
-        this.nThreads = nThreads;
-    }
-
-    public void generateWordFinders(Set<String> webAddresses) throws IOException {
-        this.wordFinderThreads.clear();
-        var iterator = webAddresses.iterator();
-        final int webAddrPerThread = webAddresses.size() / nThreads;
-        int remainingWebAddr = webAddresses.size() % nThreads;
-
-        for (int i = 0; i < nThreads; i++) {
-            WordFinderThread wf = new WordFinderThread(this.map, this.pageLinks, this.counterMonitor, this.linksMonitor);
-            wordFinderThreads.add(wf);
-
-            IntStream.range(0, webAddrPerThread).forEach(j -> wf.addWordFinder(new WordFinder(wordToFind, iterator.next())));
-            if (remainingWebAddr > 0) {
-                remainingWebAddr--;
-                wf.addWordFinder(new WordFinder(wordToFind, iterator.next()));
-            }
-        }
-    }
-
     @Override
-    public Map<String, Integer> getWordOccurences(final String webAddress, final String wordToFind, final int depth) throws ExecutionException, InterruptedException, IOException {
-        this.wordToFind = wordToFind;
-        pageLinks.add(webAddress);
-        Set<String> foundLinks = new HashSet<>();
-//        Event event;
+    public Map<String, Integer> getWordOccurences(final String webAddress, final String wordToFind, final int depth) {
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new VerticleFinder(webAddress, wordToFind, depth, res -> {
+//            Future<Map<String, Integer>> result = res;
+            System.out.println("RESULT " + res); // Ogni occorenza viene stampata
+                }))
+                .onComplete(res -> {
+                    final int viewedLinks = result.keySet().size();
+                    final int wordsFound = result.values().stream().mapToInt(Integer::intValue).sum();
 
-        for (int i = 0; i <= depth; i++) {
-                this.generateWordFinders(pageLinks);
-                wordFinderThreads.forEach(Thread::start);
-                this.linksMonitor.requestWrite();
-//                pageLinks.clear();
-                pageLinks.addAll(foundLinks);
-                this.linksMonitor.releaseWrite();
-//            event = events.poll();
-//            if (event != null) {
-//                System.out.println("EVENTO RICEVUTO");
-//                System.out.println(this.getCounter());
-//            }
-        }
+                    System.out.println("*************** REPORT ***************");
+                    System.out.println("Occurrences of \"" + wordToFind + "\" : link");
 
-        return getMap();
-    }
-
-    private int getCounter() {
-        this.counterMonitor.requestRead();
-        final int count = this.counter;
-        this.counterMonitor.releaseRead();
-        return count;
-    }
-    public Map<String, Integer> getMap() {
-        this.counterMonitor.requestRead();
-        final Map<String, Integer> map = this.map;
-        this.counterMonitor.releaseRead();
-        return map;
+                    result.forEach((k,v) -> System.out.println(v + " : " + k));
+                    System.out.println("Viewed links: " + viewedLinks);
+                    System.out.println("Words found: " + wordsFound);
+                    System.exit(0);
+                });
+        return result;
     }
 }
--- app/src/main/java/pcd/ass02/part2/test/TestWordOccurences.java
+++ app/src/main/java/pcd/ass02/part2/test/TestWordOccurences.java
@@ -10,24 +10,24 @@ import java.util.concurrent.ExecutionException;
 public class TestWordOccurences {
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
 
-        final String webAddress = "http://bias.csr.unibo.it/maltoni/ml/";
+        final String webAddress = "https://fracarlucci.github.io/RancorRank"; // https://fracarlucci.github.io/RancorRank
         final String wordToFind = "hello";
-        final int depth = 2;
+        final int depth = 1;
         final int nThreads = Runtime.getRuntime().availableProcessors();
 
-        final WordOccurrences wordOccEventLoop = new WordOccurrencesEventLoop(nThreads);
+        final WordOccurrences wordOccEventLoop = new WordOccurrencesEventLoop();
 //        final WordOccurrences wordOccVirtualThread = new WordOccurrencesVirtualThread();
 //        final WordOccurrences wordOccReactive = new WordOccurrencesReactive();
 
-        final Map<String, Integer> report = wordOccEventLoop.getWordOccurences(webAddress, wordToFind, depth);
-        final int viewedLinks = report.keySet().size();
-        final int wordsFound = report.values().stream().mapToInt(Integer::intValue).sum();
-
-        System.out.println("*************** REPORT ***************");
-        System.out.println("Occurrences of \"" + wordToFind + "\" : link");
-
-        report.forEach((k,v) -> System.out.println(v + " : " + k));
-        System.out.println("Viewed links: " + viewedLinks);
-        System.out.println("Words found: " + wordsFound);
+        Map<String, Integer> report = wordOccEventLoop.getWordOccurences(webAddress, wordToFind, depth);
+//        final int viewedLinks = report.keySet().size();
+//        final int wordsFound = report.values().stream().mapToInt(Integer::intValue).sum();
+//
+//        System.out.println("*************** REPORT ***************");
+//        System.out.println("Occurrences of \"" + wordToFind + "\" : link");
+//
+//        report.forEach((k,v) -> System.out.println(v + " : " + k));
+//        System.out.println("Viewed links: " + viewedLinks);
+//        System.out.println("Words found: " + wordsFound);
     }
 }
