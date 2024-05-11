34ef2a0b44508265de63ec949559b4a0290fc882
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2024-05-11 18:53:43 +0200

    implement a first kind of reactive program

---
+++ app/src/main/java/pcd/ass02/part2/lib/reactiveThread/WordOccurrencesReactive.java
@@ -0,0 +1,102 @@
+package pcd.ass02.part2.lib.reactiveThread;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import pcd.ass02.part2.lib.EventLoop.Monitor;
+import pcd.ass02.part2.lib.EventLoop.WordFinderThread;
+import pcd.ass02.part2.lib.WordOccurrences;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class WordOccurrencesReactive implements WordOccurrences {
+  private int counter = 0;
+  private String wordToFind;
+
+
+  private final Map<String, Integer> map = new HashMap<>();
+  private final Set<String> pageLinks = new HashSet<>();
+  Document doc;
+  //    private final Deque<Event> events;
+  private Monitor counterMonitor = new Monitor();
+  private Monitor linksMonitor = new Monitor();
+
+  @Override
+  public Map<String, Integer> getWordOccurences(final String webAddress, final String wordToFind, final int depth) throws ExecutionException, InterruptedException, IOException {
+    this.wordToFind = wordToFind;
+    pageLinks.add(webAddress);
+    final List<Future<Map.Entry<String, Integer>>> wordFutureList = new ArrayList<>();
+    final List<Future<Set<String>>> linkFutureList = new ArrayList<>();
+    Set<String> foundLinks = new HashSet<>();
+
+
+    for (int i = 0; i <= depth; i++) {
+      int finalI = i;
+      @NonNull Flowable<Set<String>> obs2 = Flowable.fromCallable(() -> {
+        Set<String> depthlinks = new HashSet<>();
+        pageLinks.forEach(address -> {
+          Set<String> links = new HashSet<>();
+          try {
+            final Document doc = Jsoup.connect(address).get();
+            links = findLinks(doc);
+            final Integer counter = findWord(doc);
+            map.put(address, counter);
+            depthlinks.addAll(links);
+          } catch (Exception e) {
+            System.out.println(e);
+          }
+        });
+        System.out.println("Level: " + finalI + ", completed.");
+        return depthlinks;
+      }).observeOn(Schedulers.io());
+      obs2.blockingSubscribe(res -> {
+        foundLinks.addAll(res);
+        pageLinks.clear();
+        pageLinks.addAll(res);
+        System.out.println("Done.");
+      });
+    }
+
+    return getMap();
+  }
+
+  public Map<String, Integer> getMap() {
+    this.counterMonitor.requestRead();
+    final Map<String, Integer> map = this.map;
+    this.counterMonitor.releaseRead();
+    return map;
+  }
+
+  private Set<String> findLinks(final Document document) {
+    return document.getElementsByTag("a")
+            .stream()
+            .map(l -> l.attr("href"))
+            .filter(l -> l.startsWith("http"))
+            .collect(Collectors.toSet());
+  }
+
+  private Integer findWord(final Document document) {
+    counter = 0;
+    Elements elements = document.body().select("*");
+    for (Element element : elements) {
+      if (Pattern.compile(Pattern.quote(this.wordToFind), Pattern.CASE_INSENSITIVE).matcher(element.ownText()).find()) {
+        String[] words = element.ownText().split("[\\s\\p{Punct}]+");
+        for (String word : words) {
+          if (word.equalsIgnoreCase(this.wordToFind)) {
+            counter++;
+          }
+        }
+      }
+    }
+    return counter;
+  }
+}
--- app/src/main/java/pcd/ass02/part2/test/TestWordOccurences.java
+++ app/src/main/java/pcd/ass02/part2/test/TestWordOccurences.java
@@ -2,6 +2,7 @@ package pcd.ass02.part2.test;
 
 import pcd.ass02.part2.lib.WordOccurrences;
 import pcd.ass02.part2.lib.EventLoop.WordOccurrencesEventLoop;
+import pcd.ass02.part2.lib.reactiveThread.WordOccurrencesReactive;
 import pcd.ass02.part2.lib.virtualThread.WordOccurrencesVirtualThread;
 
 import java.io.IOException;
@@ -9,26 +10,26 @@ import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 public class TestWordOccurences {
-    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+  public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
 
-        final String webAddress = "https://www.pizzeria-corso.it/";
-        final String wordToFind = "pizza";
-        final int depth = 2;
-        final int nThreads = Runtime.getRuntime().availableProcessors();
+    final String webAddress = "https://servizi.comune.cesena.fc.it/applicazioniweb/bacheca_informagiovani/";
+    final String wordToFind = "cesena";
+    final int depth = 2;
+    final int nThreads = Runtime.getRuntime().availableProcessors();
 
 //        final WordOccurrences wordOccEventLoop = new WordOccurrencesEventLoop(nThreads);
-        final WordOccurrences wordOccVirtualThread = new WordOccurrencesVirtualThread(nThreads);
-      /* final WordOccurrences wordOccReactive = new WordOccurrencesReactive(); */
+//        final WordOccurrences wordOccVirtualThread = new WordOccurrencesVirtualThread(nThreads);
+    final WordOccurrences wordOccReactive = new WordOccurrencesReactive();
 
-        final Map<String, Integer> report = wordOccVirtualThread.getWordOccurences(webAddress, wordToFind, depth);
-        final int viewedLinks = report.keySet().size();
-        final int wordsFound = report.values().stream().mapToInt(Integer::intValue).sum();
+    final Map<String, Integer> report = wordOccReactive.getWordOccurences(webAddress, wordToFind, depth);
+    final int viewedLinks = report.keySet().size();
+    final int wordsFound = report.values().stream().mapToInt(Integer::intValue).sum();
 
-        System.out.println("*************** REPORT ***************");
-        System.out.println("Occurrences of \"" + wordToFind + "\" : link");
+    System.out.println("*************** REPORT ***************");
+    System.out.println("Occurrences of \"" + wordToFind + "\" : link");
 
-        report.forEach((k,v) -> System.out.println(v + " : " + k));
-        System.out.println("Viewed links: " + viewedLinks);
-        System.out.println("Words found: " + wordsFound);
-    }
+    report.forEach((k, v) -> System.out.println(v + " : " + k));
+    System.out.println("Viewed links: " + viewedLinks);
+    System.out.println("Words found: " + wordsFound);
+  }
 }
