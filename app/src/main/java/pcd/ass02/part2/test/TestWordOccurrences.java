c75eb07bb12b7609143dcfd442a7c12daf5fe853
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2024-05-13 00:18:58 +0200

    Fix bug

--- app/src/main/java/pcd/ass02/part2/test/TestWordOccurrences.java
+++ app/src/main/java/pcd/ass02/part2/test/TestWordOccurrences.java
@@ -1,6 +1,11 @@
 package pcd.ass02.part2.test;
 
+import pcd.ass02.part2.lib.WordOccurrences;
 import pcd.ass02.part2.lib.eventLoop.WordOccurrencesEventLoop;
+import pcd.ass02.part2.lib.reactiveThread.WordOccurrencesReactive;
+import pcd.ass02.part2.lib.virtualThread.WordOccurrencesVirtualThread;
+
+import java.util.Map;
 
 public class TestWordOccurrences {
     public static void main(String[] args) {
@@ -9,19 +14,20 @@ public class TestWordOccurrences {
         final String wordToFind = "romagna";
         final int depth = 3;
 
-        final WordOccurrencesEventLoop wordOccEventLoop = new WordOccurrencesEventLoop();
-//        final WordOccurrences wordOccVirtualThread = new WordOccurrencesVirtualThread();
-//        final WordOccurrences wordOccReactive = new WordOccurrencesReactive();
+//        final WordOccurrencesEventLoop wordOccEventLoop = new WordOccurrencesEventLoop();
+        final WordOccurrences wordOccVirtualThread = new WordOccurrencesVirtualThread();
+        final WordOccurrences wordOccReactive = new WordOccurrencesReactive();
 
-        wordOccEventLoop.getWordOccurences(webAddress, wordToFind, depth);
-//        final int viewedLinks = report.keySet().size();
-//        final int wordsFound = report.values().stream().mapToInt(Integer::intValue).sum();
-////
-//        System.out.println("*************** REPORT ***************");
-//        System.out.println("Occurrences of \"" + wordToFind + "\" : link");
-////
-//        report.forEach((k,v) -> System.out.println(v + " : " + k));
-//        System.out.println("Links: " + viewedLinks);
-//        System.out.println("Words found: " + wordsFound);
+//        wordOccEventLoop.getWordOccurences(webAddress, wordToFind, depth);
+        final Map<String, Integer> report = wordOccVirtualThread.getWordOccurences(webAddress, wordToFind, depth);
+        final int viewedLinks = report.keySet().size();
+        final int wordsFound = report.values().stream().mapToInt(Integer::intValue).sum();
+//
+        System.out.println("*************** REPORT ***************");
+        System.out.println("Occurrences of \"" + wordToFind + "\" : link");
+//
+        report.forEach((k,v) -> System.out.println(v + " : " + k));
+        System.out.println("Links: " + viewedLinks);
+        System.out.println("Words found: " + wordsFound);
     }
 }
